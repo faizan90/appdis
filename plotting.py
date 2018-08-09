735e608568ac0887d243b64743145a9b2af8784e
Faizan_Uni <faizananwar2006@gmail.com>
2018-08-09 14:07:50 +0200

    peel and alt_peel plotting

--- plotting.py
+++ plotting.py
@@ -10,6 +10,8 @@ import h5py
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.colors import Colormap
+from matplotlib.cm import cmap_d
 
 plt.ioff()
 
@@ -23,7 +25,12 @@ class AppearDisappearPlot:
 
         self._h5_path_set_flag = False
         self._out_dir_set_flag = False
-        self._fig_size_set_flag = False
+        self._fig_props_set_flag = False
+        self._bs_vars_loaded_flag = False
+        self._bef_plot_vars_set = False
+        self._upld_pltd_flag = False
+        self._pld_pltd_flag = False
+        self._alt_pltd_flag = False
         self._in_vrfd_flag = False
         return
 
@@ -55,16 +62,29 @@ class AppearDisappearPlot:
         self._out_dir_set_flag = True
         return
 
-    def set_fig_size(self, fig_size):
+    def set_fig_props(self, fig_size, n_ticks, cmap):
 
         assert isinstance(fig_size, (tuple, list))
         assert len(fig_size) == 2
         assert (fig_size[0] > 0) and np.isfinite(fig_size[0])
         assert (fig_size[1] > 0) and np.isfinite(fig_size[1])
 
-        self._fgs = fig_size
+        assert isinstance(n_ticks, int)
+        assert (n_ticks > 0) and np.isfinite(n_ticks)
 
-        self._fig_size_set_flag = True
+        assert isinstance(cmap, (str, Colormap))
+
+        self._fgs = fig_size
+        self._nticks = n_ticks
+
+        if isinstance(cmap, str):
+            assert cmap in cmap_d
+            self._cmap = plt.get_cmap(cmap)
+
+        else:
+            self._cmap = cmap
+
+        self._fig_props_set_flag = True
         return
 
     def verify(self):
@@ -75,12 +95,39 @@ class AppearDisappearPlot:
         self._in_vrfd_flag = True
         return
 
+    def _load_bs_vars(self):
+        bsds = self._h5_hdl['boot_arrs']
+
+        self._upld_bs_ul = bsds['upld_ul'][...]
+        self._upld_bs_ll = bsds['upld_ll'][...]
+#         self._upld_bs_flg = bsds['upld_flg'][...]
+
+        if (self._ans_stl == 'peel') or (self._ans_stl == 'alt_peel'):
+
+            self._pld_bs_ul = bsds['pld_ul'][...]
+            self._pld_bs_ll = bsds['pld_ll'][...]
+#             self._pld_bs_flg = bsds['pld_flg'][...]
+
+            if self._ans_stl == 'alt_peel':
+                self._pld_upld_bs_ul = bsds['pld_upld_ul'][...]
+                self._pld_upld_bs_ll = bsds['pld_upld_ll'][...]
+#                 self._pld_upld_bs_flg = bsds['pld_upld_flg'][...]
+
+                self._upld_pld_bs_ul = bsds['upld_pld_ul'][...]
+                self._upld_pld_bs_ll = bsds['upld_pld_ll'][...]
+#                 self._upld_pld_bs_flg = bsds['upld_pld_flg'][...]
+
+        self._bs_vars_loaded_flag = True
+        return
+
     def _bef_plot(self):
 
         assert self._in_vrfd_flag
 
-        if not self._fig_size_set_flag:
+        if not self._fig_props_set_flag:
             self._fgs = (5, 5)
+            self._nticks = 15
+            self._cmap = plt.get_cmap('jet')
 
         self._h5_hdl = h5py.File(str(self.h5_path), mode='r')
 
@@ -101,7 +148,7 @@ class AppearDisappearPlot:
         self._twt = ds.attrs['twt']
         self._ans_stl = ds.attrs['ans_stl']
         self._ans_dims = ds.attrs['ans_dims']
-#         self._pl_dth = ds.attrs['pl_dth']
+        self._pl_dth = ds.attrs['pl_dth']
 #         self._n_cpus = ds.attrs['n_cpus']
 #         self._out_dir = ds.attrs['out_dir']
         self._bs_flag = ds.attrs['bs_flag']
@@ -123,32 +170,13 @@ class AppearDisappearPlot:
                 self._upld_pld = rds['upld_pld'][...]
 
         if self._bs_flag:
-            bsds = self._h5_hdl['boot_arrs']
+            self._load_bs_vars()
+            assert self._bs_vars_loaded_flag
 
-            self._upld_bs_ul = bsds['upld_ul'][...]
-            self._upld_bs_ll = bsds['upld_ll'][...]
-            self._upld_bs_flg = bsds['upld_flg'][...]
-
-            if (self._ans_stl == 'peel') or (self._ans_stl == 'alt_peel'):
-
-                self._pld_bs_ul = bsds['pld_ul'][...]
-                self._pld_bs_ll = bsds['pld_ll'][...]
-                self._pld_bs_flg = bsds['pld_flg'][...]
-
-                if self._ans_stl == 'alt_peel':
-                    self._pld_upld_bs_ul = bsds['pld_upld_ul'][...]
-                    self._pld_upld_bs_ll = bsds['pld_upld_ll'][...]
-                    self._pld_upld_bs_flg = bsds['pld_upld_flg'][...]
-
-                    self._upld_pld_bs_ul = bsds['upld_pld_ul'][...]
-                    self._upld_pld_bs_ll = bsds['upld_pld_ll'][...]
-                    self._upld_pld_bs_flg = bsds['upld_pld_flg'][...]
-
+        self._bef_plot_vars_set = True
         return
 
-    def plot_app_dis(self):
-
-        self._bef_plot()
+    def _plot_app_dis(self, plot_arr, out_fig_name):
 
         plt.figure(figsize=self._fgs)
         plt.axes().set_aspect('equal', 'box')
@@ -162,7 +190,7 @@ class AppearDisappearPlot:
                 slice(vals[0] - 0.5, vals[-1] + 0.5 + 2 - self._ws, 1),
                 slice(vals[0] - 0.5, vals[-1] + 0.5 + 2 - self._ws, 1)]
 
-            plt.pcolormesh(xcs, ycs, self._upld * 100)
+            plt.pcolormesh(xcs, ycs, plot_arr * 100, cmap=self._cmap)
 
             plt.xlabel('Reference year')
             plt.ylabel('Test year')
@@ -186,7 +214,7 @@ class AppearDisappearPlot:
 
             xcs, ycs = np.meshgrid(x, x)
 
-            plt.pcolormesh(xcs, ycs, self._upld * 100)
+            plt.pcolormesh(xcs, ycs, plot_arr * 100, cmap=self._cmap)
 
             n_x_labs = 15
             n_tick_vals = x.shape[0] - 1
@@ -203,14 +231,18 @@ class AppearDisappearPlot:
 
             add_ttl = f'''
             Window size: {self._ws} month(s)
-            Starting, ending month(s): {vals[0]}, {vals[-1]}'''
+            Starting, ending month(s): {labs[0]}, {labs[-1]}'''
+
+        else:
+            raise NotImplementedError
 
         ttl = f'''
         Appearing and disappearing situations
 
         Analysis style: {self._ans_stl}
+        Window type: {self._twt}
         {self._ans_dims} dimensions analyzed
-        {self._n_uvecs} unit vectors
+        {self._n_uvecs:1.0E} unit vectors
         {add_ttl}
         '''
 
@@ -220,8 +252,51 @@ class AppearDisappearPlot:
 
         plt.colorbar(label='Percentage', orientation='horizontal', shrink=0.5)
 
-        out_fig_name = 'upld_plot.png'
-
         plt.savefig(str(self._out_dir / out_fig_name), bbox_inches='tight')
+        plt.close()
+        return
 
+    def plot_unpeeled_app_dis(self):
+
+        if not self._bef_plot_vars_set:
+            self._bef_plot()
+
+        if not self._upld_pltd_flag:
+            self._plot_app_dis(self._upld, 'upld_plot.png')
+
+        self._upld_pltd_flag = True
+        return
+
+    def plot_peeled_app_dis(self):
+
+        if not self._bef_plot_vars_set:
+            self._bef_plot()
+
+        assert self._pl_dth
+        assert (self._ans_stl == 'peel') or (self._ans_stl == 'alt_peel')
+
+#         self.plot_unpeeled_app_dis()
+
+        if not self._pld_pltd_flag:
+            self._plot_app_dis(self._pld, 'pld_plot.png')
+
+        self._pld_pltd_flag = True
+        return
+
+    def plot_alternate_peel_app_dis(self):
+
+        if not self._bef_plot_vars_set:
+            self._bef_plot()
+
+        assert self._pl_dth
+        assert self._ans_stl == 'alt_peel'
+
+#         self.plot_peeled_app_dis()
+
+        if not self._alt_pltd_flag:
+
+            self._plot_app_dis(self._pld_upld, 'plt_upld_plot.png')
+            self._plot_app_dis(self._upld_pld, 'upld_pld_plot.png')
+
+        self._alt_pltd_flag = True
         return
--- test_app_dis.py
+++ test_app_dis.py
@@ -9,6 +9,8 @@ import time
 import pickle
 from pathlib import Path
 
+import matplotlib.pyplot as plt
+
 from appdis import (
     AppearDisappearData,
     AppearDisappearSettings,
@@ -22,66 +24,73 @@ def main():
 
     in_var_file = main_dir / r'ecad_pp_anomaly_pca_1961_2015.pkl'
 
-    n_uvecs = int(1e3)
+    n_uvecs = int(1e2)
     n_cpus = 'auto'
     fig_size = (15, 14)
-    n_dims = 6
-    ws = 10 * 12  # window size
-    analysis_style = 'raw'
-    time_win_type = 'month'
+    n_dims = 3
+    ws = 10  # window size
+    analysis_style = 'alt_peel'
+    time_win_type = 'year'
+    n_ticks = 20
+    cmap = 'jet'
 
     n_boots = 0
+
+    ann_flag = False
+    ann_flag = True
 
     out_dir = (f'anom_pca_{n_uvecs:1.0E}_uvecs_{n_dims}_dims_{ws}_ws_'
                f'{analysis_style}_as_{time_win_type}_twt_{n_boots}_bs')
 
-    peel_depth = 0  # greater than this are kept
+    peel_depth = 1  # greater than this are kept
 
     print('out_dir:', out_dir)
 
     with open(in_var_file, 'rb') as _hdl:
         in_var_dict = pickle.load(_hdl)
-        tot_in_var_arr = in_var_dict['pcs_arr'][:4100]
-        time_idx = in_var_dict['anomaly_var_df'].index[:4100]
+        tot_in_var_arr = in_var_dict['pcs_arr']  # [:4100]
+        time_idx = in_var_dict['anomaly_var_df'].index  # [:4100]
 #         eig_val_cum_sums = in_var_dict['eig_val_cum_sums']
 
-    ad_data = AppearDisappearData()
-    ad_data.set_data_array(tot_in_var_arr)
-    ad_data.set_time_index(time_idx)
-    ad_data.generate_and_set_unit_vectors(n_dims, n_uvecs, n_cpus)
-    ad_data.verify()
+    if ann_flag:
+        ad_data = AppearDisappearData()
+        ad_data.set_data_array(tot_in_var_arr)
+        ad_data.set_time_index(time_idx)
+        ad_data.generate_and_set_unit_vectors(n_dims, n_uvecs, n_cpus)
+        ad_data.verify()
 
-    ad_sett = AppearDisappearSettings()
-    ad_sett.set_analysis_parameters(
-        ws,
-        time_win_type,
-        analysis_style,
-        n_dims,
-        peel_depth,
-        n_cpus)
-    ad_sett.set_boot_strap_on_off(n_boots)
-    ad_sett.set_outputs_directory(out_dir)
-    ad_sett.save_outputs_to_hdf5_on_off(True)
-    ad_sett.verify()
+        ad_sett = AppearDisappearSettings()
+        ad_sett.set_analysis_parameters(
+            ws,
+            time_win_type,
+            analysis_style,
+            n_dims,
+            peel_depth,
+            n_cpus)
+        ad_sett.set_boot_strap_on_off(n_boots)
+        ad_sett.set_outputs_directory(out_dir)
+        ad_sett.save_outputs_to_hdf5_on_off(True)
+        ad_sett.verify()
 
-    ad_ans = AppearDisappearAnalysis()
-    ad_ans.set_data(ad_data)
-    ad_ans.set_settings(ad_sett)
-    ad_ans.verify()
+        ad_ans = AppearDisappearAnalysis()
+        ad_ans.set_data(ad_data)
+        ad_ans.set_settings(ad_sett)
+        ad_ans.verify()
 
-    ad_ans.cmpt_appear_disappear()
-    ad_ans.close_hdf5()
+        ad_ans.cmpt_appear_disappear()
+        ad_ans.close_hdf5()
 
-#     hdf5_path = ad_ans._h5_path
     hdf5_path = Path(out_dir) / 'app_dis_ds.hdf5'
 
     ad_plot = AppearDisappearPlot()
     ad_plot.set_hdf5(hdf5_path)
     ad_plot.set_outputs_directory(out_dir)
-    ad_plot.set_fig_size(fig_size)
+    ad_plot.set_fig_props(fig_size, n_ticks, cmap)
     ad_plot.verify()
 
-    ad_plot.plot_app_dis()
+    ad_plot.plot_unpeeled_app_dis()
+    ad_plot.plot_peeled_app_dis()
+    ad_plot.plot_alternate_peel_app_dis()
 
     return
 
